f42fcbf8fb4e228d7cb593ef4bdf605f55b8a806
Moon <kakumanusridhanalakshmi@gmail.com>
2025-09-06 20:48:33 +0530

    update:added lines to map direct reportees

---
+++ forcast_2/app/script.js
@@ -0,0 +1,13 @@
+function toggleDropdown(dataId, arrowId) {
+const nestedData = document.getElementById(dataId);
+const arrowIcon = document.getElementById(arrowId);
+if (nestedData.classList.contains('hidden')) {
+nestedData.classList.remove('hidden');
+arrowIcon.classList.remove('rotate-0');
+arrowIcon.classList.add('rotate-90');
+} else {
+nestedData.classList.add('hidden');
+arrowIcon.classList.remove('rotate-90');
+arrowIcon.classList.add('rotate-0');
+}
+}
