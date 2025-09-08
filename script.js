abadf2f56882ff8f7f45b2b7e8a655b2b82b0fe0
Moon <kakumanusridhanalakshmi@gmail.com>
2025-09-08 15:40:02 +0530

    update:updated new code in js for full spacing in widget

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function toggleDropdown(dataId, arrowId) {
         arrowIcon.classList.add('rotate-0');
     }
 }
+        // The ZOHO object is only available after the CRM's embedded app is ready.
+        // The recommended way to handle this is to use ZOHO.embeddedApp.onReady()
+        ZOHO.embeddedApp.onReady(function() {
+            // Resize the Zoho CRM iframe to fit the content
+            ZOHO.CRM.UI.Resize({
+                height: "100%",
+                width: "100%"
+            }).then(function(data){
+                console.log(data);
+            });
+        });
